3af29e51a7d0a2c51bab96d274ee7c099b30479d
Brandon Wie <brandonwie.cs@gmail.com>
2023-10-10 18:50:01 +0900

    setup docker

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM node:18 AS development
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+RUN npm run build
+
+FROM node:18 AS production
+
+ARG NODE_ENV=production
+ENV NODE_ENV=${NODE_ENV}
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+# install only production dependencies
+RUN npm install --only=production
+
+COPY . .
+# get build files from development
+COPY --from=development /usr/src/app/.next ./next
+
+CMD [ "npm", "run", "start"]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,25 @@ export default function Home() {
         <h1 className='text-3xl font-bold text-indigo-600'>
           This app is created by Next.js
         </h1>
-        <div>Step1. Dockerize</div>
+        <div className='p-2'>
+          <h2 className='text-2xl font-bold text-gray-900'>Step1. Dockerize</h2>
+          <p className='text-gray-900'>
+            Checkout my{' '}
+            <a
+              className='visited:text-indigo-600'
+              href='https://dev.to/brandonwie/docker-reactjs-tutorial-part-1-3110'
+              referrerPolicy='no-referrer'
+              target='_blank'
+            >
+              Docker React.js tutorial
+            </a>
+          </p>
+        </div>
+        <div className='p-2'>
+          <h2 className='text-2xl font-bold text-gray-900'>
+            Step2. Kubernetes
+          </h2>
+        </div>
       </div>
     </main>
   );
